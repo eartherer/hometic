00340357538c18c29768e14ca47abe2b3ee03210
Kittiwat Kosolwattanasombat <kittiwat.ko@kbtg.tech>
2020-09-19 12:07:53 +0700

    init

---
+++ server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("This is hometic")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/pair-device", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{"status":"active"}`))
+	}).Methods(http.MethodPost)
+
+	server := http.Server{
+		Addr:    "127.0.0.1:2009",
+		Handler: r,
+	}
+
+	log.Println("Starting server @")
+	log.Fatal(server.ListenAndServe())
+}
